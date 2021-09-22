56f8f54f9d5afb2aeb7d21f2ce57928d31426488
Yash Narvade <naravade.11809605@lpu.in>
2021-09-22 08:43:33 +0530

    Project Completed

--- app/src/main/java/com/roadtocode/webview/MainActivity.kt
+++ app/src/main/java/com/roadtocode/webview/MainActivity.kt
@@ -2,7 +2,9 @@ package com.roadtocode.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.URLUtil
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageView
 
@@ -13,13 +15,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val myWebView: WebView = findViewById(R.id.myWebView)
+        myWebView.webViewClient = WebViewClient()
 
         val etURL: EditText = findViewById(R.id.etURL)
 
         val btnSearch: ImageView = findViewById(R.id.btnSearch)
 
+        val btnHome: ImageView = findViewById(R.id.btnHome)
+        btnHome.setOnClickListener {
+            myWebView.loadUrl("https://www.google.com")
+        }
+
+        val btnLinkedIn: ImageView = findViewById(R.id.btnLinkedIn)
+        btnLinkedIn.setOnClickListener {
+            myWebView.loadUrl("https://www.linkedin.com")
+        }
+
+        val btnFacebook: ImageView = findViewById(R.id.btnFacebook)
+        btnFacebook.setOnClickListener {
+            myWebView.loadUrl("https://www.facebook.com")
+        }
+
         btnSearch.setOnClickListener {
-            myWebView.loadUrl(etURL.text.toString())
+            val url: String = etURL.text.toString()
+
+            if(URLUtil.isValidUrl(url))
+            {
+                myWebView.loadUrl(url)
+            }
+            else
+            {
+                myWebView.loadUrl("https://www.google.com/search?q="+url)
+            }
+
+
         }
 
         //enabling JavaScript
